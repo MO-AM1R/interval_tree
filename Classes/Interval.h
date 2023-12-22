c534f50d903ee7b2e2d67aa46aa0c7d7df5fbf45
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-12-22 08:24:04 +0200

    finish v1.0 for Interval class

--- Classes/Interval.h
+++ Classes/Interval.h
@@ -1,8 +1,23 @@
 # ifndef INTERVAL_H
 # define INTERVAL_H
 
+/**
+ * @brief {
+ *      Class Interval
+ *      represent the interval [low, high] e.g. [5, 10]
+ * }
+ * @authors Mohamed Amir
+ * @version 1.0
+ */
 class Interval {
+    int low, high ; /// low, and high represent the interval endpoints
 
+public:
+    Interval() = default ; /// default constructor
+    explicit Interval(const int l, const int h) : low(l), high(h) {} /// parameterized constructor
+
+    int getLow() const {return low ;} /// getter to get the interval low
+    int getHigh() const {return high ;} /// getter to get the interval high
 };
 
 # endif //INTERVAL_H
