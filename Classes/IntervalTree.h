6c8868fe3a3198008404da4b7c24ecd68c8547a3
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-12-22 08:45:47 +0200

    add the base structure for IntervalTree class

--- Classes/IntervalTree.h
+++ Classes/IntervalTree.h
@@ -1,8 +1,29 @@
 # ifndef INTERVALTREE_H
 # define INTERVALTREE_H
+# include "Node.h"
 
+/**
+ * @brief {
+ *      Class IntervalTree
+ *      represent the interval tree
+ *      it has an @see {class Node} @code {root}
+ * }
+ * @authors Mohamed Amir
+ * @version 1.0
+ */
 class IntervalTree {
+    Node* root ;
 
+public:
+    IntervalTree() = default; /// default constructor
+
+    void insert(const Interval interval) { /// insert function to insert new interval
+
+    }
+
+    Interval search(const Interval interval) const{ /// search function to search for the interval
+
+    }
 };
 
 # endif //INTERVALTREE_H
